a8db8c8cdd0f1e6df1863eb151fa98d2d4f15908
Marcelo <marceloarbore@gmail.com>
2025-09-15 01:30:19 -0300

    Swgundo commit. v1 15-Sept-25

---
+++ test/clojure_datomic_adb_23ai_vector_search/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-datomic-adb-23ai-vector-search.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-datomic-adb-23ai-vector-search.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
